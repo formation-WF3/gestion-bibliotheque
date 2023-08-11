a215371203ce89f25dfc2fac22561420c5e7f7c8
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2023-08-11 10:11:12 +0200

    Modification des String en Long pour les repositories.

--- backend/src/main/java/com/formation/gestionbibliotheque/repositories/CategoryRepository.java
+++ backend/src/main/java/com/formation/gestionbibliotheque/repositories/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.formation.gestionbibliotheque.repositories;
 import com.formation.gestionbibliotheque.models.CategoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<CategoryModel, String> {
+public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
 }
--- backend/src/main/java/com/formation/gestionbibliotheque/repositories/LoanRepository.java
+++ backend/src/main/java/com/formation/gestionbibliotheque/repositories/LoanRepository.java
@@ -3,5 +3,5 @@ package com.formation.gestionbibliotheque.repositories;
 import com.formation.gestionbibliotheque.models.LoanModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LoanRepository extends JpaRepository<LoanModel, String> {
+public interface LoanRepository extends JpaRepository<LoanModel, Long> {
 }
