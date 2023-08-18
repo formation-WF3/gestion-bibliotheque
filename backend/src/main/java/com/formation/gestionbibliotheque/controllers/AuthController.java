8e5333b1bf005f44b170929a4a077be0fd54818e
said benelhadj <sad.beneldje@gmail.com>
2023-08-18 09:53:36 +0200

    test

--- backend/src/main/java/com/formation/gestionbibliotheque/controllers/AuthController.java
+++ backend/src/main/java/com/formation/gestionbibliotheque/controllers/AuthController.java
@@ -33,8 +33,6 @@ import com.formation.gestionbibliotheque.repositories.UserRepository;
 import com.formation.gestionbibliotheque.security.jwt.JwtUtils;
 import com.formation.gestionbibliotheque.security.services.UserDetailsImpl;
 
-import jakarta.validation.Valid;
-
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
