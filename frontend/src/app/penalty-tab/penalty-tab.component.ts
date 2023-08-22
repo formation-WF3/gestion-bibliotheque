325fa96a2706dd249bdb47b5303acc9769107ac4
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-08-22 16:03:26 +0200

    generating penaltyComponent/ bookSearch component

---
+++ frontend/src/app/penalty-tab/penalty-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-penalty-tab',
+  templateUrl: './penalty-tab.component.html',
+  styleUrls: ['./penalty-tab.component.css']
+})
+export class PenaltyTabComponent {
+
+}
