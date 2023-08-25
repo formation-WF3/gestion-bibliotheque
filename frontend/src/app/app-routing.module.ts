178854ef3989f73a4641ccf26f9280a7786241f8
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-08-25 11:49:55 +0200

    WiP

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { BoardModeratorComponent } from './board-moderator/board-moderator.compo
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 import { MainBoardComponent } from './main-board/main-board.component';
+import { BookSearchComponent } from './book-search/book-search.component';
 
 
 
@@ -24,6 +25,7 @@ const routes: Routes = [
   { path: 'admin', component: BoardAdminComponent },
   { path: 'under-construction', component: UnderConstructionComponent},
   { path: 'main-board', component:MainBoardComponent},
+  { path:'book-search', component:BookSearchComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
