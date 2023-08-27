a11f9b42087e52576d91a644e6983bbb595af6e1
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-08-27 12:29:36 +0200

    binding #1

--- frontend/src/app/nav-buttons/nav-buttons.component.ts
+++ frontend/src/app/nav-buttons/nav-buttons.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nav-buttons',
   templateUrl: './nav-buttons.component.html',
-  styleUrls: ['./nav-buttons.component.css']
+  styleUrls: ['./nav-buttons.component.css'],
 })
 export class NavButtonsComponent {
 
+  @Output()
+  profileEvent = new EventEmitter<string>();
+  goToProfileEvent(value: string) {
+    this.profileEvent.emit(value);
+    console.log(value)
+  }
+/*
+  @Output()
+  loanEvent = new EventEmitter<boolean>();
+  goToLoanEvent(value: true) {
+    this.loanEvent.emit(value);
+  }
+
+  @Output()
+  penaltyEvent = new EventEmitter<boolean>();
+  goToPenaltyEvent(value: true) {
+    this.penaltyEvent.emit(value);
+  }
+  */
 }
