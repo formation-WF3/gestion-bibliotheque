77344583ac020e7e4a6ee5ac3432b4d2c0833888
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-08-27 13:45:18 +0200

    adding goToNavEvent()

--- frontend/src/app/nav-buttons/nav-buttons.component.ts
+++ frontend/src/app/nav-buttons/nav-buttons.component.ts
@@ -8,22 +8,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class NavButtonsComponent {
 
   @Output()
-  profileEvent = new EventEmitter<string>();
-  goToProfileEvent(value: string) {
-    this.profileEvent.emit(value);
+  navEvent = new EventEmitter<string>();
+  goToNavEvent(value: string) {
+    this.navEvent.emit(value);
     console.log(value)
   }
-/*
-  @Output()
-  loanEvent = new EventEmitter<boolean>();
-  goToLoanEvent(value: true) {
-    this.loanEvent.emit(value);
-  }
 
-  @Output()
-  penaltyEvent = new EventEmitter<boolean>();
-  goToPenaltyEvent(value: true) {
-    this.penaltyEvent.emit(value);
-  }
-  */
 }
