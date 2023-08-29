b7f16695f79e7a35d2b5bb98dec3fd739f925f10
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-08-29 12:06:01 +0200

    Wip

--- frontend/src/app/_services/book.service.ts
+++ frontend/src/app/_services/book.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Book } from '../models/Book';
+import { Book } from '../models/book';
 
 @Injectable({
   providedIn: 'root'
