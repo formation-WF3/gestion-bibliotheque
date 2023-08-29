430fdd66cb79909ed042a320c455d0cfd64310b2
Said developpeur java/angular <47383155+saidbenelhadj@users.noreply.github.com>
2023-08-29 17:59:48 +0200

    Develop said (#27)
    * modif const api_url
    
    * config register with validator
    
    * list books a jour

--- frontend/src/app/_services/book.service.ts
+++ frontend/src/app/_services/book.service.ts
@@ -1,21 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 
+const API_URL = "http://localhost:8080/api/";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  API_URL = "http://localhost:8080/api";
+ 
 
-  constructor( private httpClient: HttpClient) { 
+  constructor( private http: HttpClient) { 
   }
 
-  getAll(): Observable<Book[]>{ 
+  getAll(): Observable<any[]>{ 
 
-    return this.httpClient.get<Book[]>(`${this.API_URL}/books`)
+    return this.http.get<Book[]>(API_URL + 'books')
     
    }
 
