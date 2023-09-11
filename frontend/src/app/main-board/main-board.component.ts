6df3d2fdfc69e635c8f76ed7250d38b48e777b1f
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-09-11 11:34:48 +0200

    wip

--- frontend/src/app/main-board/main-board.component.ts
+++ frontend/src/app/main-board/main-board.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-main-board',
   templateUrl: './main-board.component.html',
-  styleUrls: ['./main-board.component.css']
+  styleUrls: ['./main-board.component.css'],
 })
-
 export class MainBoardComponent {
+  items = ['PROFILE', 'LOAN', 'PENALTY'];
+  buttonSelected = 'PROFILE';
 
-items=['PROFILE','LOAN','PENALTY']
-
-buttonSelected = 'PROFILE';
-
-selectComponent(navEvent:string){
- this.buttonSelected=navEvent 
-  // console.log(navEvent)
-  // console.log('essai')
-
- 
-}
-
-
+  selectComponent(navEvent: string) {
+    this.buttonSelected = navEvent;
+  }
 }
